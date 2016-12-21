b9f03566d3fafb73be787df4d5f2f857cb4fb78b
Charles Kebbi <charlieforce1999@gmail.com>
2016-12-21 07:39:07 +0000

    Added users index view and action details

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,11 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
   
-  
+# The .includes combines all your database together instead
+# of having separate tables for each user. This is the same
+# as having @users = Users.all
   def index
+    @users = User.includes(:profile)
     
   end
   
